f1e79040a1d9cdae4636a62636bae36b12453c11
Tanvir Islam streame <streametanvir@gmail.com>
2020-06-18 15:02:01 +0600

    [FIX] salary calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Cart = (props, product) => {
+const Cart = (props) => {
+    const [total, setTotal] = useState(0);
     const cart = props.cart;
-    let total = 0;
-    for(let i = 0; i < cart.length; i++){
-        const product = cart[i];
-        total = product.price;
-    }
 
-      return (
+    useEffect(() => {
+        let newTotal = 0;
+        for (let i = 0; i < cart.length; i++) {
+            newTotal += cart[i].salary;
+        }
+
+        setTotal(newTotal)
+    }, [props.cart])
+
+
+    return (
         <div>
-            <h3>Total add friend: { cart.length}</h3>
-            
+            <h3>Total add friend: {cart.length}</h3>
             <h4> Total Salary : {total}</h4>
-            </div>
-        
+        </div>
     )
 };
 
